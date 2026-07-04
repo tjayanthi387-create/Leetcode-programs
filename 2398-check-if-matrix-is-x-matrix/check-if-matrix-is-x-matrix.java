233020deb55048b071f5fdff2172333ca4ef5b85
tjayanthi387-create <tjayanthi387@gmail.com>
2026-07-04 12:52:06 +0530

    Time: 2 ms (99.57%) | Memory: 47 MB (54.70%) - LeetSync

---
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isDiagonal = (i == j) || (i + j == n - 1);
+                
+                if (isDiagonal) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                } else {
+                    if (grid[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
