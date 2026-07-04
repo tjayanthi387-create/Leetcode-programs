e25aa8fa0d0203c2081673162d52d55b80fe29f9
tjayanthi387-create <tjayanthi387@gmail.com>
2026-07-04 10:33:07 +0530

    Time: 1 ms (59.98%) | Memory: 47.8 MB (56.83%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        int[] height1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println("Example 1 Output: " + solver.trap(height1)); 
+        int[] height2 = {4, 2, 0, 3, 2, 5};
+        System.out.println("Example 2 Output: " + solver.trap(height2)); 
+    }
+}
