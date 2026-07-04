cee0aa5480f29ea36078210230d18594afbb007f
tjayanthi387-create <tjayanthi387@gmail.com>
2026-07-04 12:53:38 +0530

    Time: 1 ms (99.98%) | Memory: 46.6 MB (92.01%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {2, 2, 1};
+        int[] nums2 = {4, 1, 2, 1, 2};
+        int[] nums3 = {1};
+
+        System.out.println(singleNumber(nums1)); 
+        System.out.println(singleNumber(nums2)); 
+        System.out.println(singleNumber(nums3));
+    }
+}
+   
