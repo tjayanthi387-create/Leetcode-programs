9987bc186cfd75ddc8cec7d2a118036613a7c358
tjayanthi387-create <tjayanthi387@gmail.com>
2026-07-04 13:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (31.44%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            if (i != n - 1 - i) {
+                sum += mat[i][n - 1 - i];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] mat2 = {
+            {1, 1, 1, 1},
+            {1, 1, 1, 1},
+            {1, 1, 1, 1},
+            {1, 1, 1, 1}
+        };
+
+        int[][] mat3 = {
+            {5}
+        };
+
+        System.out.println(diagonalSum(mat1)); 
+        System.out.println(diagonalSum(mat2)); 
+        System.out.println(diagonalSum(mat3)); 
+    }
+}
